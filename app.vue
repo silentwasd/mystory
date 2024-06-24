0bc10adb07dba4f33711f68c20177fb7569bdd19
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-06-24 08:29:02 +0300

    NuxtUI!

--- app.vue
+++ app.vue
@@ -1,6 +1,29 @@
+<script setup lang="ts">
+const modalShow = ref<boolean>(false);
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container mx-auto max-w-4xl py-10">
+      <UCard>
+          <template #header>
+              <h3 class="text-xl">Test interface</h3>
+          </template>
+
+          <UFormGroup label="Place your congratulations">
+              <UInput placeholder="Wow, it's work!"/>
+          </UFormGroup>
+
+          <UButton class="mt-5" @click="modalShow = true">Yay!</UButton>
+      </UCard>
+
+      <UModal v-model="modalShow">
+          <UCard>
+              Wooow! Hooray!
+
+              <template #footer>
+                  <UButton @click="modalShow = false">OK</UButton>
+              </template>
+          </UCard>
+      </UModal>
   </div>
 </template>
